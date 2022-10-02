73b5f1681f54823fd61567ba48ea2b25804adf9a
Blessing Njoku <blessingnjoku505@gmail.com>
2022-10-02 11:15:24 +0100

    ST:quiz game

---
+++ create.py
@@ -0,0 +1,18 @@
+# different ways of concatenatin in python
+
+
+# name='Blessing Njoku'
+# print('my name is ' + name)
+# print('my name is {}' .format(name))
+# print(f"my name is {name}")
+
+# complete the quote with the missing words
+
+first_word=raw_input('its starts wit S and ends with e')
+second_word=raw_input('it starts wit f and ends with l')
+third_word=raw_input('it start with d and ends with t')
+
+quote=f"it is better to remain {first_word} at the risk of being thought a {second_word} than to talk and remove all {third_word}"
+print(quote)
+
+
---
+++ quizgame.py
@@ -0,0 +1,13 @@
+# print('welcome to logical quiz game')
+
+
+question = raw_input('are you in the mood to play the game? ')
+print(question + ' please')
+
+if question !='yes':
+    quit()
+print('thanks for accepting your first challenge')
+
+quiz_question = raw_input("capital of Nigeria is? ")
+if quiz_question == "Abuja":
+    print('correct!')
